08a39227c5319fa616c3c4ee5108e30f81a82463
zhaobb4 <zhaobb4@lenovo.com>
2022-04-29 16:09:08 +0800

    fix: improve code

--- src/app/pages/company-manage/company-manage.component.ts
+++ src/app/pages/company-manage/company-manage.component.ts
@@ -1,37 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 interface Person {
-  key: string;
+  key: number;
   name: string;
-  age: number;
-  address: string;
+  idcard: string;
+  company: string;
+  account: string;
 }
-
 @Component({
-  selector: 'company-manage',
+  selector: 'user-manage',
   templateUrl: './company-manage.component.html',
   styleUrls: ['../pages.component.less', './company-manage.component.less']
 })
-export class CompanyManageComponent {
+export class CompanyManageComponent implements OnInit {
   title = '单位管理';
-  listOfData: Person[] = [
-    {
-      key: '1',
+  
+  listOfData: Person[] = [];
+
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initList();
+
+    this.validateForm = this.fb.group({
+      idcard: [null],
+      name: [null],
+      mobile: [null]
+    });
+  }
+
+  initList = () => {
+    const arr = [];
+    const data = {
       name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park'
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park'
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park'
+      idcard: '320509*********87',
+      company: '人力资源社会保障局',
+      account: 'lizhinan'
     }
-  ];
+    for (let i = 1; i <= 50; i++) {
+      const item = {
+        key: i,
+        ...data
+      }
+      arr.push(item);
+    }
+    this.listOfData = arr;
+  }
+
+  search = () => {
+    if (this.validateForm.valid) {
+      console.log('submit', this.validateForm.value);
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+
+  newData = () => {
+
+  }
+
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    for (const key in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(key)) {
+        this.validateForm.controls[key].markAsPristine();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
+  }
+
+  importData = () => {
+
+  }
+
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -12,6 +12,34 @@ export class PagesComponent {
 
   searchValue?: string;
 
+  menus = [
+    {
+      name: '工作台',
+      icon: 'assets/images/menu_icon01.png',
+      path: '/pages/workbench',
+    },
+    {
+      name: '用户管理',
+      icon: 'assets/images/menu_icon02.png',
+      path: '/pages/user',
+    },
+    {
+      name: '单位管理',
+      icon: 'assets/images/menu_icon03.png',
+      path: '/pages/company',
+    },
+    {
+      name: '科室管理',
+      icon: 'assets/images/menu_icon04.png',
+      path: '/pages/department',
+    },
+    {
+      name: '应用管理',
+      icon: 'assets/images/menu_icon05.png',
+      path: '/pages/application',
+    },
+  ]
+
   search = () => {
     console.log(this.searchValue)
   }
